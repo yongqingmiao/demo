837307aa6fcb6105f4737704b85e1cb2cc13fdb7
v_miaoyongqin <yongqing_miao@163.com>
2016-08-17 22:15:46 +0800

    redis案例
    test

---
+++ jedis-test/src/main/java/org/my/jedis/demo/Demo2.java
@@ -0,0 +1,15 @@
+package org.my.jedis.demo;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("111");
+		System.out.println("111");
+		System.out.println("111");
+		System.out.println("111");
+		System.out.println("111");
+		
+	}
+
+}
